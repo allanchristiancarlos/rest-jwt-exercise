0b0e830ca93624fab3fecaaddd69afd008046bba
Allan Christian Carlos <allanchristiancarlos@gmail.com>
2017-06-03 08:51:39 +0800

    reused project to Bikero

--- Bikero.Api/Startup.cs
+++ Bikero.Api/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace RestJwt.Api
+namespace Bikero.Api
 {
     public class Startup
     {
---
+++ Bikero.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bikero.Domain
+{
+    public class Class1
+    {
+    }
+}
